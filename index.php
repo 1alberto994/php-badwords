b2b13c3e1968798ee9ec5e4ccfb3418ca45eede2
1alberto994 <alby94ct@gmail.com>
2023-02-17 14:06:01 +0100

    inizio esercizio

---
+++ index.php
@@ -0,0 +1,27 @@
+
+<?php
+$title='BADWORDS';
+$text='Hai rotto il cazzo';
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>badwords</title>
+</head>
+<body>
+    <h1> <?php echo $title ?> </h1>
+    <p>
+        il mio testo è:<?php echo $text ?>
+    </p>
+    <p>
+        la lunghezza del mio  testo è di : <?php echo strlen($text)?> lettere
+    </p>
+</body>
+</html>
